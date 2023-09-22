bf3a711bc59815c5f39ab3987c633c20cfd2b004
Shakila K Bandara <skbandara32@gmail.com>
2023-09-23 02:08:43 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>EcoAgriLink | Sri Lanka's Sustainable Farming Hub</title>
+
+
+    <!-- css -->
+    <link rel="stylesheet" href="CSS/style.css">
+    
+    <!--Google fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Roboto+Mono:wght@400;500;600&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Righteous">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+</head>
+<body>
+    
+    <div class="main">
+        <div class="navibar">
+
+            <div class="logo">
+                <!-- <img src="images/doa.png"> -->
+                <div class="name">Eco<label class="agri">Agri</label>Link</div>
+            </div>
+
+            <div class="links">
+                <div class="link_item"><a href="#">Home</a></div>
+                <div class="link_item"><a href="#">About Us</a></div>              
+                <div class="link_item"><a href="#">Services</a></div>
+                <div class="link_item">
+                    <div class="account-dropdown"><a href="#">Account</a>     
+                        <div class="dropdown-content">
+                            <div class="link_item"><a href="#"><i class="bi bi-person-fill"></i> Sign In</a></div>
+                            <div class="link_item"><a href="#"><i class="bi bi-person-fill-add"></i> Sign Up</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="link_item">
+                    <div class="search-dropdown"><a href="#"><i class="bi bi-search"></i></a>
+                        <div class="search">
+                            <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+                              </form>
+                        </div>
+                    </div>
+                </div>              
+            </div>
+        </div>
+
+        <div class="headline">
+            <label class="headline-text">Discover Sustainable Farming with the Department of Agriculture.</label>
+            <a href="#" class="learn-more">Learn More &nbsp;<i class="bi bi-arrow-right-circle"></i><i class="bi bi-arrow-right-circle-fill"></i></a>
+        </div>
+        
+    </div>
+
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
